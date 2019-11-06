5a3d4de6d1ead6845091a704b041ebdd5a514188
Anton Mashko <anton.mashko@gmail.com>
2019-11-06 21:53:25 +0200

    fixed sed in fixname script

--- fixname.sh
+++ fixname.sh
@@ -7,6 +7,6 @@ fname=$(basename $(pwd))
 echo 'applying new name='$fname' in import path and another resources'
 
 find * -type f \( ! -name '*.sh' \) -exec \
-    sed -i 's/$tmname/$fname/g' {} +
+    sed -i "s/$tpname/$fname/g" {} +
 
 echo 'done. now you can remove this file'
